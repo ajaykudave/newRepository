8b53e978f885400669eba63e5ba7c0564ed02754
Ajay Kudave <jaykudave@gmail.com>
2022-10-15 12:26:22 +0530

    added add function

--- operations.js
+++ operations.js
@@ -1,3 +1,9 @@
 console.log('GitHub Operations,changes on master branch');
-let a =10,b=20;
-console.log(`Addition is ${a+b}`);
+ let a =10,b=20;
+
+/* console.log(`Addition is ${a+b}`); */
+function add(){
+    console.log(`Addition is ${a+b}`);
+}
+
+add(a,b);
